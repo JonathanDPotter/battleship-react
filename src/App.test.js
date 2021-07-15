610c385108071cde83a493e5584c7e1a563dacf4
JonathanDPotter <jonathan.d.potter@outlook.com>
2021-07-15 16:56:47 -0400

    testing tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App { state }from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(state.thing).toBe(5);
+
 });
